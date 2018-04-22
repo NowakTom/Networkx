149fd78ab9cedd7701c84609bb78272c14137f9a
Tomek Nowak <nowak_tomek@o2.pl>
2018-04-22 23:27:26 +0200

    nie dziala rysowanie

--- loading_test.py
+++ loading_test.py
@@ -6,3 +6,14 @@ from urllib.request import urlretrieve
 website = "https://raw.githubusercontent.com/NowakTom/Networkx/master/out_ucidata-gama.txt"
 urlretrieve(website, "out_ucidata-gama.txt")
 
+f1 = csv.reader(open("out_ucidata-gama.txt","r"))
+for row in f1: G.add_nodes_from(row[0], color = 'blue')
+
+for row in f1: G.add_nodes_from(row[1], color = 'red')
+
+for row in f1: G.add_edge(row[0],row[1])
+
+color_map = []
+
+for n in G.nodes(): color_map.append(G.node[n]['color']) nx.draw_networkx(G, node_color = color_map, with_labels = True, node_size = 500)
+

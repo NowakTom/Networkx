97034fc23c7e505d1a8dad6f0de5dc0a9ed8fa24
Tomek Nowak <nowak_tomek@o2.pl>
2018-07-24 22:26:15 +0200

    t

--- AHC_test.py
+++ AHC_test.py
@@ -11,81 +11,40 @@ def entropy(X):
      return _entropy
 
 G=nx.Graph()
-#G1=nx.Graph()
-#G2=nx.Graph()
-dict_colors = []
-#_dict2 = []
-#nodes_colors = []
-edges_weights = []
-d_G1_G2 = []
+color_list = []
 
 G = nx.random_graphs.erdos_renyi_graph(31,0.3)
 
-#for node in G:
-#	if node % 3 == 0:
-#		G1.add_node(node, color = 'white')
-#	else:
-#		G2.add_node(node, color = 'black')
+node_colors = { i: np.random.choice(['white','black']) for i in G.nodes}
 
-#G1_color=nx.get_node_attributes(G1,'color')
-#G2_color=nx.get_node_attributes(G2,'color')	
-
-#for node in G1:
-#		dict_colors.append('white')
-
-#for node in G2:
-#		_dict2.append('black')
-
-#nodes_colors = dict_colors + _dict2
-
-#for node in G:
-#	if node % 3 == 0:
-#		G1.add_node(node, color = 'white')
-#	else:
-#		G2.add_node(node, color = 'black')
-
-edges_weights_summary = np.zeros(len(G))
-
-for node in G:
-	if node % 3 == 0:
-		dict_colors.append('white')
-	else:
-		dict_colors.append('black')
+nx.set_node_attributes(G, node_colors, 'color')
 
 for u,v in G.edges:	
-	if dict_colors[u] == dict_colors[v]:
-		edges_weights.append(1)
+	if  G.nodes[u]['color'] ==  G.nodes[v]['color']:
+		G[u][v]['weight']= 1
 	else:
-		edges_weights.append(2)
-		
-for u,v in G.edges:	
-	if u == u:
-		edges_weights_summary[u] = edges_weights_summary[u] + edges_weights[u]
+		G[u][v]['weight']= 2
 
-for x in edges_weights_summary:	
-	if x != 0:
-		d_G1_G2.append(1/x * entropy(dict_colors))
-	else:
-		d_G1_G2.append(0)
+group = [[i] for i in G]
 
-#d_G1_G2 = 1/sum(edges_weights) * entropy(dict_colors)
-		
-print("Graph nodes")
-print(list(G.nodes()))
-print("Nodes colors")
-print(dict_colors)
-print("Entropy")
-print(entropy(dict_colors))
-print("Edges weights")
-print(edges_weights)
-print("Nodes - edges weights")
-print(edges_weights_summary)
-print("Nodes distance")
-print(d_G1_G2)
 
-X = [[i] for i in d_G1_G2]
+#for node in G:	
+#	print(G.nodes[node]['color'])
 
-Z = linkage(X, 'single')
-fig = plt.figure(figsize=(25, 10))
-dn = dendrogram(Z)
-plt.show()
+print(group)
+#print(entropy(G.nodes[i]['color']))
+
+for i in G.nodes:	
+	color_list.append(G.nodes[i]['color'])
+
+#1/x * entropy(dict_colors
+
+
+#Z = linkage(X, 'single')
+print(color_list)
+print(entropy(color_list))
+#print(G.degree(weight = 'weight')[0])
+#print(len(G.degree))
+#print(len(color_list))
+
+#print(G.degree(weight = 'weight')[0])
